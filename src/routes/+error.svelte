9862455ac1963edb4fe41492195b843e2f011145
Eli Frigo <ex.games.org@gmail.com>
2024-01-18 20:16:04 -0600

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+</script>
+
+<div class="container">
+	<h1 class="status">{$page.status}</h1>
+	{#if $page.error != null}
+		<h2 class="message">{$page.error.message}</h2>
+	{/if}
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		text-align: center;
+	}
+	h1.status {
+		font-size: 4em;
+		margin-bottom: 0.1em;
+	}
+	h2.message {
+		font-size: 2em;
+		margin-top: 0em;
+	}
+</style>
