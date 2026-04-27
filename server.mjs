52239ca02451f32169f8bc066b440bbab1cbfedf
Eli Frigo <eli@elifrigo.com>
2026-04-27 11:46:32 -0500

    Remove server.mjs wrapper, fixed upstream in SvelteKit

--- server.mjs
+++
@@ -1,9 +0,0 @@
-process.on('uncaughtException', (err) => {
-    console.error('Uncaught exception:', err);
-});
-
-process.on('unhandledRejection', (reason) => {
-    console.error('Unhandled rejection:', reason);
-});
-
-import('./build/index.js');
